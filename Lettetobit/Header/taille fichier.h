f57fe1134d684d2b146b9ccd0a466d99873b0edd
Joseph2505 <73166056+Joseph2505@users.noreply.github.com>
2020-12-11 16:26:45 +0100

    Ajout du header avec les commentaires
    Ajout des commentaires dans le header

--- Lettetobit/Header/taille fichier.h
+++ Lettetobit/Header/taille fichier.h
@@ -1,6 +1,19 @@
+/*****************************************************************//**
+ * \file   taille fichier.h
+ * \brief  header allowing to read a text file and retrieve the size of that file
+ * 
+ * \author josephxu joseph.xu@efrei.net
+ * \date   December 2020
+ *********************************************************************/
+
 #ifndef HUFFMAN_1_H
 #define HUFFMAN_1_H
 
+/**
+ * \brief Function that returns the size of the file so the number of characters.
+ * \param a pointer to the char with the name of the file where we will calculate the size.
+ * \return an int which is the number of characters in the text file.
+ */
 int taille_fichier(char *);
 
 #endif //HUFFMAN_1_H
